5265595b601910628dd3df49e76437ecb36edf09
fearfully-m <kmstevensj@gmail.com>
2025-07-22 01:40:21 -0400

    added photo generator for metadata testing

---
+++ main.py
@@ -0,0 +1,11 @@
+from PIL import Image # for creating images
+import os # to handle files and metadata 
+import time # to get access to the time metadata
+import random
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
